a91595bc1981ee1a7c995efa61e79335eda68a6f
Josue M. Polanco-Martinez <josue.m.polanco@gmail.com>
2015-08-04 00:00:00 +0000

    version 2.0

--- R/WCC.R
+++ R/WCC.R
@@ -1,49 +1,33 @@
 WCC <-
-function(inputDATA, Wname, J, lmax, Hpdf, Wpdf) { 
+function(inputDATA, Wname, J, lmax, device="screen", filename, 
+          Hfig, WFig, Hpdf, Wpdf) { 
 
-  if(is.ts(inputDATA) != "TRUE")
-  stop("The input data is not a time series, please check the ts 
-  function in the R manual pages. Bye, thank you for your interest 
-  in our program. \n")
+  #: Check the input data. 
+  if (is.ts(inputDATA) != "TRUE")
+    stop("The input data is not a time series, please check the ts 
+    function in the R manual pages. Bye, thank you for your interest 
+    in our program. \n")
 
   namesSMI  <- colnames(inputDATA)
   cat("colnames", namesSMI) 
   NL        <- dim(inputDATA)[1]
 
-##Function from SOWAS (Maraun & Kurths 2004)###
- wtcolors <- function(ncolors){
-     upside <- rainbow(ncolors,start=0,end=.7)
-     down   <- upside
-     for (i in 1:ncolors) {
-      down[i] <- upside[ncolors+1-i]
-     }
-    down
-   }
-  ncolors  <- 128 
-  colors   <- wtcolors(ncolors)
-
-  #:: Param. para ourgraph wav. cros. cor.
+  #:: Param.  to ourgraph wav. cros. cor.
   xx  <- seq(-lmax, lmax, 1)
   Nx  <- length(xx)
   yy  <- 1:J
   Ny  <- length(yy)
 
-  #:: The auxiliary barcolour function  
-  rangecol <- (0:(ncolors-1)) / (ncolors-1)
-  byseq    <- floor(ncolors/10)
-  atlab    <- seq(2, length(rangecol), by=byseq)
-  Lrangcol <- rangecol[atlab]
+  modwt.inputDATAx  <- modwt(inputDATA[,1], Wname, n.levels = J)
+  modwt.inputDATAy  <- modwt(inputDATA[,2], Wname, n.levels = J) 
+  bw.modwinputDATAx <- brick.wall(modwt.inputDATAx, Wname)
+  bw.modwinputDATAy <- brick.wall(modwt.inputDATAy, Wname)
  
-   modwt.inputDATAx  <- modwt(inputDATA[,1], Wname, n.levels = J)
-   modwt.inputDATAy  <- modwt(inputDATA[,2], Wname, n.levels = J) 
-   bw.modwinputDATAx <- brick.wall(modwt.inputDATAx, Wname)
-   bw.modwinputDATAy <- brick.wall(modwt.inputDATAy, Wname)
- 
- returns.cross.cor <- NULL
-   for(i in 1:J) {
-    wcc <- spin.correlation(bw.modwinputDATAx[[i]],
+  returns.cross.cor <- NULL
+   for (i in 1:J) {
+     wcc <- spin.correlation(bw.modwinputDATAx[[i]],
              bw.modwinputDATAy[[i]], lmax)
-    returns.cross.cor <- cbind(returns.cross.cor, wcc)
+     returns.cross.cor <- cbind(returns.cross.cor, wcc)
    }
 
    returns.cross.cor           <- ts(as.matrix(returns.cross.cor),
@@ -56,44 +40,69 @@ function(inputDATA, Wname, J, lmax, Hpdf, Wpdf) {
    upper.ci <- tanh(atanh(returns.cross.cor) + qnorm(0.975) /
                 sqrt(matrix(trunc(NL/2^(1:J)), nrow=lags, ncol=J,
                 byrow=TRUE) - 3))
- 
 
    #: plot the WCC 
    mat.zero <- array(NaN, c(Nx, Ny))
    setlinM  <- array(NaN, c(J, 1))
    for (i in 1:J) {
-    idx <- which ( upper.ci[,i] < 0 | lower.ci[,i] > 0 )
-    mat.zero[idx,i] <- returns.cross.cor[idx,i]
-    setlinM[i,1] <- which(returns.cross.cor[,i] == max(returns.cross.cor[,i]))
+       idx <- which ( upper.ci[,i] < 0 | lower.ci[,i] > 0 )
+       mat.zero[idx,i] <- returns.cross.cor[idx,i]
+       setlinM[i,1]    <- which(returns.cross.cor[,i] == max(returns.cross.cor[,i]))
    }
-   labv     <- xx[setlinM[,1]]
-   X1       <- labv
-   X2       <- labv
-   p51      <- seq(0.5, J, 1) 
-   p52      <- seq(1.5, J+1, 1) 
-   rangev   <- seq(min(returns.cross.cor), max(returns.cross.cor),
-                   length.out=ncolors)
-   rangebar <- matrix(rangev,nrow=1,ncol=ncolors,byrow=TRUE)
-   Lrangbar <- rangebar[atlab]
 
-   VEClab <- seq(-lmax, lmax, 5)
-   VECJ   <- rep(0, J) 
-   j <- 1:J
-   VECJ <- 2^(j-1) 
-   pdf(file=paste("./wcc_", namesSMI[1],  namesSMI[2], ".pdf", 
-    sep=""), width = Wpdf, height = Hpdf)
-    layout(matrix(c(1,2), ncol=2, byrow=TRUE), widths=c(4,1))
-    image(xx, yy, z=mat.zero, col=colors, xlab="Lag (days)",
-     ylab="Scale", main=paste(namesSMI[1], "vs.", namesSMI[2]), yaxt="n")
-    segments(X1, p51, X2, p52, lty=2, lwd=2)
-    abline(v=VEClab, lty=3, lwd=2)
-    axis(2, at=1:J, labels=VECJ)
-    image(z=rangebar, axes=FALSE, col=colors, frame.plot=TRUE,
-     yaxt="n", xaxt="n") 
-    axis(2, at=Lrangcol, labels=round(Lrangbar, digits=2), las=2)
-   dev.off()
+  #:: Palette!
+  Ncol     <- length(xx)*J*8
+  Palette  <- diverge_hcl(Ncol, c=c(100,0), l=c(50,90), power=1.3)
+  #:: colorbar! 
+  rangev   <- seq(min(mat.zero, na.rm=TRUE), max(mat.zero, na.rm=TRUE), 
+               length.out=J+1)
+  rangebar <- matrix(rangev, nrow=1, ncol=J+1, byrow=TRUE)
+
+  labv    <- xx[setlinM[,1]]
+  X1      <- labv
+  X2      <- labv
+  p51     <- seq(0.5, J, 1) 
+  p52     <- seq(1.5, J+1, 1) 
+  VEClab  <- seq(-lmax, lmax, 5)
+  VECJ    <- rep(0, J) 
+  j       <- 1:J
+  VECJ    <- 2^(j-1) 
+
+  ## Devices options: png & jpg; esp & pdf! 
+  if (device=="png") {
+   fileout <- paste("WCC_", filename, ".png", sep="") 
+   png(fileout, height=Hfig, width=WFig) 
+  } 
+
+  if (device=="jpeg" || device=="jpg") {
+   fileout <- paste("WCC_", filename, ".jpg", sep="") 
+   jpeg(fileout, height=Hfig, width=WFig) 
+  } 
+
+  if (device=="pdf") {
+   fileout <- paste("WCC_", filename, ".pdf", sep="") 
+   pdf(fileout, height=Hpdf, width=Wpdf)
+  }
+
+  if (device=="eps") {
+   fileout <- paste("WCC_", filename, ".eps", sep="") 
+   postscript(fileout, height=Hpdf, width=Wpdf)
+  }
+
+   layout(matrix(c(1,2), ncol=2, byrow=TRUE), widths=c(4,1))
+   image(xx, yy, z=mat.zero, col=Palette, xlab="Lag (days)",
+    ylab="Scale", main=paste(namesSMI[1], "vs.", namesSMI[2]), yaxt="n")
+   segments(X1, p51, X2, p52, lty=2, lwd=2)
+   abline(v=VEClab, lty=3, lwd=2)
+   axis(2, at=1:J, labels=VECJ)
+   image(z=rangebar, axes=FALSE, col=Palette, frame.plot=TRUE,
+    yaxt="n", xaxt="n") 
+   axis(2, at=round(seq(0,1,length.out=J+1),2), labels=round(rangebar, 
+    digits=2), las=2)
+
+  if (device != "screen") 
+  dev.off()
 
   return(list(returns.cross.cor=returns.cross.cor))
 
   }
-
--- R/WMC.R
+++ R/WMC.R
@@ -1,5 +1,6 @@
 WMC <-
-function(inputDATA, Wname, J, Hpdf, Wpdf) { 
+function(inputDATA, Wname, J, device="screen", filename,
+          Hfig, WFig, Hpdf, Wpdf) { 
   
  if(is.ts(inputDATA) != "TRUE")
   stop("The input data is not a time series, please check the ts 
@@ -27,8 +28,27 @@ function(inputDATA, Wname, J, Hpdf, Wpdf) {
   returns.modwt.cor <- LS$xy.mulcor[1:J,]
   YmaxR             <- LS$YmaxR
 
-  #:: Generating the pdf plot file! 
-  pdf("./WMC_plot.pdf")
+   ## Devices options: png & jpg; esp & pdf! 
+  if (device=="png") {
+   fileout <- paste("WMC_", filename, ".png", sep="")
+   png(fileout, height=Hfig, width=WFig)
+  }
+
+  if (device=="jpeg" || device=="jpg") {
+   fileout <- paste("WMC_", filename, ".jpg", sep="")
+   jpeg(fileout, height=Hfig, width=WFig)
+  }
+
+  if (device=="pdf") {
+   fileout <- paste("WMC_", filename, ".pdf", sep="")
+   pdf(fileout, height=Hpdf, width=Wpdf)
+  }
+
+  if (device=="eps") {
+   fileout <- paste("WMC_", filename, ".eps", sep="")
+   postscript(fileout, height=Hpdf, width=Wpdf)
+  }
+
   par(mfrow=c(1,1), las=0, mar=c(5,4,4,2)+.1)
   matplot(2^(0:(J-1)), returns.modwt.cor[-(J+1),], type="b",
   log="x", pch="*LU", xaxt="n", lty=1, col=c(1,4,4),
@@ -37,6 +57,8 @@ function(inputDATA, Wname, J, Hpdf, Wpdf) {
   axis(side=1, at=2^(0:7))
   abline(h=0)
   text(2^(0:7),rep(-0.1,J),labels=NAMES[YmaxR[-1]],adj=0.5,cex=.85)
+
+  if (device != "screen")
   dev.off()
   
   cat("labels",  NAMES[YmaxR[-1]])
@@ -44,4 +66,3 @@ function(inputDATA, Wname, J, Hpdf, Wpdf) {
   return(list(LS=LS))
 
 }
-
